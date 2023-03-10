00246eceec49af94296e914114f9c5eb2ecce81d
fazt <fazt@faztweb.com>
2023-03-10 00:40:27 -0500

    variable examples

---
+++ variables/pitagorean_theorem.py
@@ -0,0 +1,16 @@
+"""
+This script determines the Pythagorean Distance
+between three given points
+"""
+
+# Initialize variables
+x, y, z = 2, 3, 4
+
+# Pythagorean Theorem in 3 dimensions
+w_squared = x**2 + y**2 + z**2
+
+# The square root gives the distance
+w = w_squared ** 0.5
+
+#Show the distance
+print(w)
